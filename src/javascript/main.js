ca98982b65466d3d942d220012e917ba204c9fba
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-09 13:19:28 -0300

    add-more-elements-in-main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -9,6 +9,8 @@ async function checkWeather(){
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
 }
 
 checkWeather();
