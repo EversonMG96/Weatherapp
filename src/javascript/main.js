fdb782f14732b51b7636d88014a8153a4fafcd4b
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-09 13:26:05 -0300

    adjust-parameters-in-ain.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,14 +1,16 @@
 const apiKey = "d514009f1874431b33661655dfef1fe9";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 }
