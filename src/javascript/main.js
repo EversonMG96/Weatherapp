6f4c7f1090be08bc950c37b28fa8fa399620336b
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-09 13:09:26 -0300

    add functions in js and using api

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,5 +2,10 @@ const apiKey = "d514009f1874431b33661655dfef1fe9";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
 async function checkWeather(){
-    
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
 }
+
+checkWeather();
