97ad63a00edb3ba1cd00594377fe5f7f6ae25e96
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-09 12:58:51 -0300

    create const in javascript

---
+++ src/javascript/main.js
@@ -0,0 +1,6 @@
+const apiKey = "d514009f1874431b33661655dfef1fe9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather(){
+    
+}
