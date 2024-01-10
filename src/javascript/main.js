9d0df6bfccd4d42b79d046ce8e8aaaf77090561f
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-10 13:31:31 -0300

    adding last line in main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -37,6 +37,7 @@ async function checkWeather(city) {
         }
     
         document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     
     } 
 
