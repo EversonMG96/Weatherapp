49f24e40ddd71c6aa67280fa9916b602efba725d
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-10 02:10:05 -0300

    create space for news config

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -14,6 +14,9 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+
+    
 }
 
 searchBtn.addEventListener("click", ()=>{
